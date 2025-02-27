0055cb8420fbc8337f9e4eab593961f49103afa8
Pawel Kawa <ramzord@gmail.com>
2025-02-27 10:52:09 +0000

    added relationships

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
-    //
+	protected $fillable = [
+		'name',
+		'description',
+		'start_time',
+		'end_time',
+		'location',
+	];
+
+	protected $casts = [
+		'start_time' => 'datetime',
+		'end_time' => 'datetime',
+	];
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+	public function attendees(): HasMany
+	{
+		return $this->hassMany(Attendee::class);
+	}
 }

ed384289e1f03ed6f2c49aa21b61d4a74eed12e0
Pawel Kawa <ramzord@gmail.com>
2025-03-15 11:52:26 +0000

    Add UserResource and AttendeeResource to EventResource

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class EventController extends Controller
 {
@@ -13,7 +15,7 @@ class EventController extends Controller
      */
     public function index()
     {
-		return Event::all();
+		return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -31,15 +33,21 @@ class EventController extends Controller
 			]),
 			'user_id' => 1,
 		]);
-		return $event;
+		return new EventResource($event);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+	public function show(Event $event)
     {
-		return Event::findOrFail($id);
+		try {
+			//code...
+			$event->load('user', 'attendees');
+			return new EventResource($event);
+		} catch (\Throwable $th) {
+			Log::info($th);
+		}
     }
 
     /**
@@ -56,7 +64,7 @@ class EventController extends Controller
 				'location' => 'sometimes|string|max:255',
 			])
 		);
-		return $event;
+		return new EventResource($event);
 	}
 
     /**
